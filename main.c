caebb7e9a20d26b1ffb2541866e1950cb56af4c7
Aleksei.Tirman <aleksei.tirman@jetbrains.com>
2025-09-15 09:59:06 +0300

    Experiment

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_render.h>
 #include <SDL3/SDL_main.h>
+#include <emscripten/html5.h>
+#include <stdio.h>
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
+bool window_resize(int eventType, const EmscriptenUiEvent *uiEvent, void *userData) {
+    return true;
+}
+
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
+
+    // emscripten_set_resize_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW, NULL, false, window_resize);
+
+    // int w, h;
+    // emscripten_get_canvas_element_size("#canvas", &w, &h);
+
+    // printf("%d %d\n", w, h);
+
+
     if (!SDL_CreateWindowAndRenderer("Hello World", 800, 600, SDL_WINDOW_FULLSCREEN, &window, &renderer)) {
         SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
+
     return SDL_APP_CONTINUE;
 }
 
@@ -22,22 +39,26 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
-    const char *message = "Hello!";
+    const char *message = "Hello22!";
     int w = 0, h = 0;
     float x, y;
     const float scale = 4.0f;
 
     /* Center the message and scale it up */
     SDL_GetRenderOutputSize(renderer, &w, &h);
-    SDL_SetRenderScale(renderer, scale, scale);
-    x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
-    y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
+    // printf("%d %d\n", w, h);
+    // SDL_SetRenderScale(renderer, scale, scale);
+    // x = ((w / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE * SDL_strlen(message)) / 2;
+    // y = ((h / scale) - SDL_DEBUG_TEXT_FONT_CHARACTER_SIZE) / 2;
 
     /* Draw the message */
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 222, 0, 255);
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderDebugText(renderer, x, y, message);
+    SDL_FRect r = {600, 595, 130.0f, 140.0f};
+    bool ret = SDL_RenderFillRect(renderer, &r);
+    // SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // SDL_RenderDebugText(renderer, x, y, message);
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
