9bf31dd1c4615d3b11d158cd2afda2317edabe27
Aleksei Tirman <aleksei.tirman@jetbrains.com>
2025-09-16 15:33:41 +0300

    Add helper to render text

--- src/main.c
+++ src/main.c
@@ -9,15 +9,32 @@
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
-static TTF_Font *font = NULL;
-static SDL_Texture *texture = NULL;
+static TTF_Font *title_font = NULL;
 static int dpr = 1;
+
+static SDL_Texture *game_title_tex = NULL;
+static SDL_Color text_color = { 255, 255, 255, 255 };
 
 EM_JS(void, env_settings, (int *width, int *height, int *dpr), {
     HEAP32[dpr / 4] = window.devicePixelRatio || 1;
     HEAP32[width / 4] = window.innerWidth;
     HEAP32[height / 4] = window.innerHeight;
 });
+
+static SDL_Texture *create_text_texture(TTF_Font *f, const char *text, SDL_Color color) {
+    SDL_Surface *surf = TTF_RenderText_Blended(f, text, 0, color);
+    SDL_Texture *texture = NULL;
+    if (surf) {
+        texture = SDL_CreateTextureFromSurface(renderer, surf);
+        SDL_DestroySurface(surf);
+    }
+
+    return texture;
+}
+
+static TTF_Font *load_font(const char *path, float ptsize) {
+    return TTF_OpenFont("ShareTechMono-Regular.ttf", ptsize * (float) dpr);
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
@@ -34,26 +51,13 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
         return SDL_APP_FAILURE;
     }
 
-    font = TTF_OpenFont("ShareTechMono-Regular.ttf", 32.0f * (float) dpr);
-
-    if (!font) {
-        SDL_Log("Couldn't open font: %s\n", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
-
-    SDL_Color color = { 255, 255, 255, SDL_ALPHA_OPAQUE };
-    SDL_Surface *text = TTF_RenderText_Blended(font, "0123456789", 0, color);
-    if (text) {
-        texture = SDL_CreateTextureFromSurface(renderer, text);
-        SDL_DestroySurface(text);
-    }
-
-    if (!texture) {
-        SDL_Log("Couldn't create text: %s\n", SDL_GetError());
-        return SDL_APP_FAILURE;
-    }
+    title_font = load_font("ShareTechMono-Regular.ttf", 20.0f);
+    game_title_tex = create_text_texture(title_font, "ECHOES OF THE DEEP", text_color);
 
     SDL_SetRenderScale(renderer, (float) dpr, (float) dpr);
+
+    SDL_Rect rect = { 20, 20, window_width - 20, window_height - 20 };
+    SDL_SetRenderViewport(renderer, &rect);
 
     if (pool_init(32 * 1024 * 1024) < 0) {
         SDL_Log("Cannot allocate memory");
@@ -71,19 +75,16 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
-    int w = 0, h = 0;
     SDL_FRect dst;
-    SDL_GetTextureSize(texture, &dst.w, &dst.h);
-    SDL_GetRenderOutputSize(renderer, &w, &h);
-    const float scale = 4.0f;
-    dst.x = 100;
-    dst.y = 100;
-    dst.w /= ((float) dpr - 0.2f);
+    SDL_GetTextureSize(game_title_tex, &dst.w, &dst.h);
+    dst.x = 0;
+    dst.y = 0;
+    dst.w /= ((float) dpr);
     dst.h /= (float) dpr;
 
     SDL_SetRenderDrawColor(renderer, 21, 24, 27, 255);
     SDL_RenderClear(renderer);
-    SDL_RenderTexture(renderer, texture, NULL, &dst);
+    SDL_RenderTexture(renderer, game_title_tex, NULL, &dst);
     SDL_RenderPresent(renderer);
 
     return SDL_APP_CONTINUE;
